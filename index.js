6feb96c228f78cfb9fa6d5bb9a3a2eca0b9d71ca
Hugh Kennedy <hughskennedy@gmail.com>
2018-04-13 21:58:44 +0100

    Improvements for controller

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const Sections = require('./lib/sections')
 const Controls = require('./lib/controls')
 
 const HOST = window.location.protocol + '//' + window.location.host + '/player'
-const client = require('socket.io-client')(HOST)
+const client = require('socket.io-client')(HOST, {
+  transports: ['websocket', 'polling']
+})
 
 console.log(HOST)
 
@@ -17,14 +19,13 @@ const changeSection = Sections('intro')
 const controls = Controls(client, document.querySelector('section[name=controller]'))
 
 client.once('connect', function () {
-  // introSubtitle.innerHTML = 'tap to continue...'
-  setTimeout(changeSection.bind(null, 'controller'))
-  // window.addEventListener('click', tapToContinue, false)
-  // window.addEventListener('ontouchstart', tapToContinue, false)
-  // window.addEventListener('ontouchend', tapToContinue, false)
-
   client.on('client:color', function (color) {
     document.body.style.backgroundColor = 'rgb(' + [color.red, color.green, color.blue].join(', ') + ')'
+
+    client.emit('client:roundtrip', client.id, function () {
+      client.hasDoneRoundTrip = true
+      setTimeout(changeSection.bind(null, 'controller'))
+    })
   })
 })
 
@@ -34,56 +35,3 @@ function tapToContinue (e) {
   window.removeEventListener('ontouchstart', tapToContinue, false)
   window.removeEventListener('ontouchend', tapToContinue, false)
 }
-
-
-  // var orientation = null
-  // window.onclick = null
-  // var baseEuler = null
-
-  // h1.innerHTML = 'clicked'
-  // new FULLTILT.getDeviceOrientation({
-  //   type: 'world'
-  // }).then(function (controller) {
-  //   orientation = controller
-  //   h1.innerHTML = 'orientation accessed'
-  // }).catch(function (e) {
-  //   alert(e.message || e)
-  // })
-
-  // var axes = [0, 0]
-  // var data = {
-  //   id: null,
-  //   type: null,
-  //   xInput: 0,
-  //   yInput: 0,
-  // }
-
-  // draw()
-  // function draw () {
-  //   requestAnimationFrame(draw)
-
-  //   if (!orientation) return
-  //   if (!client.id) return
-
-  //   var euler = orientation.getScreenAdjustedEuler()
-
-  //   if (!baseEuler) {
-  //     baseEuler = [euler.gamma, euler.beta]
-  //   }
-
-  //   axes[0] = clamp((euler.gamma - baseEuler[0]) / 50)
-  //   axes[1] = -clamp(((euler.beta - baseEuler[1])) / 50)
-
-  //   data.id = client.id
-  //   data.type = 'tilt'
-  //   data.xInput = axes[0]
-  //   data.yInput = axes[1]
-
-  //   client.emit('client:input', data)
-
-  //   h2.innerHTML = JSON.stringify(data)
-  // }
-
-  // function clamp (a) {
-  //   return Math.max(-1, Math.min(a, +1))
-  // }
--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const http = require('http')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio(server)
+const io = socketio(server, {
+  wsEngine: 'ws',
+  transports: ['websocket', 'polling']
+})
 
 app.use(express.static(path.resolve(__dirname, 'static')))
 
-server.listen(80, (err) => {
+server.listen(3000, (err) => {
   if (err) throw err
   console.log('http://localhost:3000/')
 })
@@ -18,24 +21,58 @@ server.listen(80, (err) => {
 const channelPlayer = io.of('/player')
 const colors = []
 
-channelPlayer.on('connection', (client) => {
-  var color = getRandomColor()
+const colorLUT = {}
+delete colorLUT.x
 
+channelPlayer.on('connection', (client) => {
+  var color = colorLUT[client.id] = getRandomColor()
+
+  // broadcast color to game clients
   io.emit('client:connect', toRGB(client.id, color))
-  client.emit('client:color', toRGB(client.id, color))
+
+  // detect disconnection and broadcast to game
   client.on('disconnect', () => {
+    delete colorLUT[color]
     colors.push(color)
     io.emit('client:disconnect', client.id)
   })
 
+  // broadcast input events
   client.on('client:input', (data) => {
-    //console.log(data)
     io.emit('client:input', data)
+  })
+
+  // pass color to client on first connection
+  client.emit('client:color', toRGB(client.id, color))
+
+  // roundtrip request for confirming the connection is working
+  client.on('client:roundtrip', (data, next) => {
+    next()
   })
 })
 
-io.on('connection', (client) => {
-  console.log("HOST CONNECTED", client.id, client.nsp.name)
+io.on('connection', (server) => {
+  console.log("HOST CONNECTED", server.id, server.nsp.name)
+
+  server.on('server:populate', (_, next) => {
+    var players = io.of('player').connected
+
+    console.log('Game started!')
+    console.log('Existing players and their colours requested')
+
+    for (var id in players) {
+      if (!colorLUT[id]) continue
+      server.emit('client:connect', toRGB(id, colorLUT[id]))
+    }
+
+    server.once('disconnect', () => {
+      console.log('disconnected :O')
+    })
+  })
+
+  server.on('server:keepalive', () => {
+    console.log('server keepalive fired')
+  })
 })
 
 function getRandomColor () {
